d7a109b43cc0f5f4683287e480214d519eeae048
JamesTheGeek <radjame52004@gmail.com>
2025-03-18 12:38:45 -0400

    work on quote object

---
+++ src/main/java/dev/mydogsed/sollexicalanalyzer/commands/quotes/util/Quote.java
@@ -0,0 +1,29 @@
+package dev.mydogsed.sollexicalanalyzer.commands.quotes.util;
+
+public class Quote {
+
+    /*
+     Alright, so...
+     what is a quote?
+
+    - can be divided into two parts
+
+    the CONTENT:
+    the actual content of the quote. this is either text, or an image. the text may contain emoji, stickers, etc.
+
+    the META:
+    the person who submitted the quote
+    -> username, id, image, etc.
+    -> time submitted
+    -> link to the message
+     */
+
+    // Quote Content
+    // should be the raw content of the quote
+     private String content;
+
+     
+
+
+
+}
